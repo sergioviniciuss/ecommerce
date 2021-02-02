cdce8852f4486e4a54fa2059ba37826553353092
sergioviniciuss <sergioviniciuss@gmail.com>
2021-02-02 22:16:37 +0100

    Feat(Main): Creates boilerplate page

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,7 +1,12 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Atom image - Advanced React" />
+    <S.Title>Advanced React</S.Title>
+    <S.Description>Typescript, ReactJS, NextJS and Styled Components</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="A dev in front of the computer" />
+  </S.Wrapper>
 )
 
 export default Main
