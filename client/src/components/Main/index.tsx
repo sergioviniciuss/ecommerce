6c7340cbfc1182fda6440bea6d9230c49973b215
sergioviniciuss <sergioviniciuss@gmail.com>
2021-02-04 20:58:29 +0100

    Feat(Storybook): Adds Knobs addon to allow customization of props directly in the storybook

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,11 +1,17 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Advanced React',
+  description = 'Typescript, ReactJS, NextJS and Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo src="/img/logo.svg" alt="Atom image - Advanced React" />
-    <S.Title>Advanced React</S.Title>
-    <S.Description>Typescript, ReactJS, NextJS and Styled Components</S.Description>
-    <S.Illustration src="/img/hero-illustration.svg" alt="A dev in front of the computer" />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A dev in front of the computer"
+    />
   </S.Wrapper>
 )
 
--- client/src/components/Main/stories.tsx
+++ client/src/components/Main/stories.tsx
@@ -1,8 +1,18 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  decorators: [withKnobs]
 }
 
-export const Basic = () => <Main />
+export const Basic = () => (
+  <Main
+    title={text('Title', 'Advanced React')}
+    description={text(
+      'Description',
+      'Typescript, ReactJS, NextJS and Styled Components'
+    )}
+  />
+)
