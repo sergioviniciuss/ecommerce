90d0672f0935d80cec53fb1816dda2f256f5ba75
sergioviniciuss <sergioviniciuss@gmail.com>
2021-02-04 21:27:19 +0100

    Feat(Storybook): Removes knobs and other addons and uses controls instead

--- client/src/components/Main/stories.tsx
+++ client/src/components/Main/stories.tsx
@@ -1,19 +1,9 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'Advanced React')}
-    description={text(
-      'Description',
-      'Typescript, ReactJS, NextJS and Styled Components'
-    )}
-  />
-)
+export const Default: Story = (args) => <Main {...args} />
