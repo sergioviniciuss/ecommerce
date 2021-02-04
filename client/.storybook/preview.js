0522740dea5313d7394353e66ada585514d9a2d2
sergioviniciuss <sergioviniciuss@gmail.com>
2021-02-04 20:41:53 +0100

    Feat(Storybook): Introduces GlobalStyles as decorator so the fonts/styles gets applied to the stories

---
+++ client/.storybook/preview.js
@@ -0,0 +1,10 @@
+// .storybook/preview.js
+
+import { addDecorator } from '@storybook/react'
+import withGlobalStyles from './withGlobalStyles'
+
+addDecorator(withGlobalStyles)
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
